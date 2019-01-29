62041bbfdb44f2ce9a7f5c1d3293413dd38fc100
aBetterFun <fanmh1996@gmail.com>
2019-01-29 15:57:01 -0500

    v1.0

--- p1/part1/client.cpp
+++ p1/part1/client.cpp
@@ -23,56 +23,60 @@ int main(int argc, char const *argv[])
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(8080);
 
-	inet_pton(AF_INET, "18.221.175.74", &serv_addr.sin_addr);
+	inet_pton(AF_INET, "129.97.167.80", &serv_addr.sin_addr);
 
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
 		printf("\nConnection Failed \n");
 		return -1;
 	}
-
+/*
 	//For Q1
-	//for(int i=0;i<100;i++){
-	//	gettimeofday(&start, NULL);
-	//	send(sock,"1",1,0);
-	//	read(sock, readin, sizeof(readin));
-	//	gettimeofday(&end, NULL);
-	//	secs  = end.tv_sec  - start.tv_sec;
-	//	usecs = end.tv_usec - start.tv_usec;
-	//	mtime = ((secs) * 1000 + usecs/1000.0);
-	//	printf("%f\n", mtime);
-	//}
-	
+	for(int i=0;i<100;i++){
+		gettimeofday(&start, NULL);
+		send(sock,"1",1,0);
+		read(sock, readin, sizeof(readin));
+		gettimeofday(&end, NULL);
+		secs  = end.tv_sec  - start.tv_sec;
+		usecs = end.tv_usec - start.tv_usec;
+		mtime = ((secs) * 1000 + usecs/1000.0);
+		printf("%f\n", mtime);
+	}
+	send(sock, "end", 3, 0);
+*/
+/*	
 	//For Q2
-	//for (int j = 0; j<100;j++){
-	//	gettimeofday(&start, NULL);
-	//	for (int i=0;i<10;i++){
-	//		send(sock, buffer, sizeof(buffer), 0);
-	//	}
-	//	send(sock, "END", 3, 0);
-	//	read(sock, readin, sizeof(readin));
-	//	gettimeofday(&end, NULL);
-	//	secs  = end.tv_sec  - start.tv_sec;
-	//	usecs = end.tv_usec - start.tv_usec;
-	//	mtime = ((secs) * 1000 + usecs/1000.0);
-	//	printf("%f\n", mtime);
-	//}
-	
+	for (int j = 0; j<100;j++){
+		gettimeofday(&start, NULL);
+		for (int i=0;i<10;i++){
+			send(sock, buffer, sizeof(buffer), 0);
+		}
+		send(sock, "END", 3, 0);
+		read(sock, readin, sizeof(readin));
+		gettimeofday(&end, NULL);
+		secs  = end.tv_sec  - start.tv_sec;
+		usecs = end.tv_usec - start.tv_usec;
+		mtime = ((secs) * 1000 + usecs/1000.0);
+		printf("%f\n", mtime);
+	}
+	send(sock, "end", 3, 0);
+*/
+/*
 	//For Q3 E1
-	//for (int j=0; j<100;j++){
-	//	sock = socket(AF_INET, SOCK_STREAM, 0);
-	//	gettimeofday(&start, NULL);
-	//	connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	//	send(sock,"1",1,0);
-	//	read(sock, readin, sizeof(readin));
-	//	gettimeofday(&end, NULL);
-	//	secs  = end.tv_sec  - start.tv_sec;
-	//	usecs = end.tv_usec - start.tv_usec;
-	//	mtime = ((secs) * 1000 + usecs/1000.0);
-	//	printf("%f\n", mtime);
-	//	send(sock, "end", 3, 0);
-	//}
-	
+	for (int j=0; j<100;j++){
+		sock = socket(AF_INET, SOCK_STREAM, 0);
+		gettimeofday(&start, NULL);
+		connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+		send(sock,"1",1,0);
+		read(sock, readin, sizeof(readin));
+		gettimeofday(&end, NULL);
+		secs  = end.tv_sec  - start.tv_sec;
+		usecs = end.tv_usec - start.tv_usec;
+		mtime = ((secs) * 1000 + usecs/1000.0);
+		printf("%f\n", mtime);
+		send(sock, "end", 3, 0);
+	}
+*/	
 	//For Q3 E2
 	for (int j=0;j<100;j++){
 		sock = socket(AF_INET, SOCK_STREAM, 0);
